d50f47db33aee669338ad18f020f863861c3b03c
goyaschooling <goyaschooling@gmail.com>
2017-09-06 10:52:05 -0700

    Add All Files

---
+++ ChallengeConditionalRadioButton/Default_CC_Radiobutton.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default_CC_Radiobutton.aspx.cs" Inherits="ChallengeConditionalRadioButton.Default_CC_Radiobutton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Your Note Taking Preferences<br />
+            <br />
+            <asp:RadioButton ID="pencilRadioButton" runat="server" GroupName="noteTaking" Text="Pencil" />
+            <br />
+            <asp:RadioButton ID="penRadioButton" runat="server" GroupName="noteTaking" Text="Pen" />
+            <br />
+            <asp:RadioButton ID="phoneRadioButton" runat="server" GroupName="noteTaking" Text="Phone" />
+            <br />
+            <asp:RadioButton ID="tabletRadioButton" runat="server" GroupName="noteTaking" Text="Tablet" />
+            <br />
+            <br />
+            <asp:Button ID="okButton1" runat="server" OnClick="okButton1_Click" Text="OK" />
+            <br />
+            <br />
+            <asp:Image ID="prefImage" runat="server" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeConditionalRadioButton/Default_CC_Radiobutton.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default_CC_Radiobutton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton1_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+
+            if (pencilRadioButton.Checked)
+            {
+                prefImage.ImageUrl = "https://openclipart.org/download/216413/coniglio_rabbit_small.svg";
+                resultLabel.Text = "You selected Pencil.";
+            }
+            else if (penRadioButton.Checked)
+            {
+                prefImage.ImageUrl = "https://openclipart.org/download/214840/smallgroup.svg";
+                resultLabel.Text = "You selected Pen.";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                prefImage.ImageUrl = "https://openclipart.org/download/214840/smallgroup.svg";
+                resultLabel.Text = "You selected Phone.";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                prefImage.ImageUrl = "https://openclipart.org/download/214840/smallgroup.svg";
+                resultLabel.Text = "You selected Tablet.";
+            }
+            else
+            {
+                resultLabel.Text = "Select an option.";
+            }
+
+
+        }
+    }
+}
